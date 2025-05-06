61ce4defc293fe70876a6277613d2f2c7ce95362
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-05-06 11:06:27 +0300

    feat : Add abstract method declarations.

--- erpnext/data/custom_import/data_import.py
+++ erpnext/data/custom_import/data_import.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from frappe import _
 
 class DataImport(ABC):
@@ -10,3 +10,18 @@ class DataImport(ABC):
         self.valid = True
         self.filename = filename
         self.line_number = 0
+
+    @abstractmethod
+    def check_integrity(self):
+        """Check the integrity of the data"""
+        pass
+
+    @abstractmethod
+    def check_foreign_key(self):
+        """Check foreign key references"""
+        pass
+
+    @abstractmethod
+    def insert_data(self):
+        """Insert data into the system"""
+        pass
