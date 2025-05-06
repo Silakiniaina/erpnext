88c00a20359f4ae5dddcc24b5618ad26578cef22
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-05-06 11:18:50 +0300

    feat : Create function check_type.

--- erpnext/data/custom_import/supplier_import.py
+++ erpnext/data/custom_import/supplier_import.py
@@ -1,5 +1,6 @@
 from erpnext.data.custom_import.data_import import DataImport
 
+from frappe import _
 
 class SupplierImport(DataImport):
     """
@@ -11,3 +12,22 @@ class SupplierImport(DataImport):
         self.country = country
         self.supplier_type = supplier_type
         self.additional_data = {}
+
+    def check_type(self):
+        """
+        Check if supplier type exists in the system
+        
+        Returns:
+            bool: True if valid, False otherwise
+        """
+        if not self.check_required_value(self.supplier_type, "supplier_name"):
+            self.errors.append(f"Line {self.line_number}: Supplier Type is required")
+            self.valid = False
+            return
+            
+        supplier_type_exists = frappe.db.exists("Supplier Type", self.supplier_type)
+        if not supplier_type_exists:
+            self.errors.append(f"Line {self.line_number}: Supplier Type '{self.supplier_type}' does not exist")
+            self.valid = False
+            pass
+        pass
